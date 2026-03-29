7f020abfdbe7b74d7b410054836d7be80a16982c
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-03-30 00:52:31 +0530

    feat: update Azure resources including log analytics, container registry, and app environment

---
+++ imports.tf
@@ -0,0 +1,29 @@
+import {
+  to = azurerm_log_analytics_workspace.workspaceaicloudbuilder9db5
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.OperationalInsights/workspaces/workspaceaicloudbuilder9db5"
+}
+
+import {
+  to = azurerm_container_registry.spiritops
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.ContainerRegistry/registries/spiritops"
+}
+
+import {
+  to = azurerm_app_managed_environment.spiritops_container_app_env
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env"
+}
+
+import {
+  to = azurerm_container_app.spiritops_app
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/containerApps/spiritops-app"
+}
+
+import {
+  to = azurerm_app_managed_certificate.spiritops_managed_certificate
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env/managedCertificates/www.spiritops.in-spiritop-260227063125"
+}
+
+import {
+  to = azurerm_dns_zone.spiritops_in
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.Network/dnszones/spiritops.in"
+}

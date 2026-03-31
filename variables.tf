07593c79ad012dd4cce0c7544ac6d7da0139c983
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-01 00:02:26 +0530

    feat: update Azure resources including Log Analytics, Container Registry, and App Services

--- variables.tf
+++ variables.tf
@@ -1,13 +1,19 @@
+variable "log_analytics_workspace_name" {
+  description = "The name of the Log Analytics workspace."
+  type        = string
+  default     = "workspaceaicloudbuilder9db5"
+}
+
 variable "container_registry_name" {
-  description = "The name of the Azure Container Registry."
+  description = "The name of the Container Registry."
   type        = string
   default     = "spiritops"
 }
 
-variable "log_analytics_workspace_name" {
-  description = "The name of the Log Analytics Workspace."
+variable "container_registry_sku" {
+  description = "The SKU of the Container Registry."
   type        = string
-  default     = "workspaceaicloudbuilder9db5"
+  default     = "Basic"
 }
 
 variable "container_app_name" {
@@ -16,10 +22,22 @@ variable "container_app_name" {
   default     = "spiritops-app"
 }
 
-variable "app_service_environment_name" {
-  description = "The name of the App Service Environment."
+variable "managed_environment_name" {
+  description = "The name of the Managed Environment."
   type        = string
   default     = "spiritops-container-app-env"
+}
+
+variable "managed_certificate_name" {
+  description = "The name of the Managed Certificate."
+  type        = string
+  default     = "www.spiritops.in-spiritop-260227063125"
+}
+
+variable "certificate_hostname" {
+  description = "The hostname for the Managed Certificate."
+  type        = string
+  default     = "www.spiritops.in"
 }
 
 variable "dns_zone_name" {
@@ -28,14 +46,14 @@ variable "dns_zone_name" {
   default     = "spiritops.in"
 }
 
+variable "dns_zone_location" {
+  description = "The location of the DNS Zone."
+  type        = string
+  default     = "global"
+}
+
 variable "location" {
   description = "The Azure location where resources will be created."
   type        = string
   default     = "southindia"
-}
-
-variable "container_registry_sku" {
-  description = "The SKU of the Container Registry."
-  type        = string
-  default     = "Basic"
 }

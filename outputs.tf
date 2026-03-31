9db46dfeda8a5c5314ea650741713a7749c9598e
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-03-31 23:53:49 +0530

    feat: update Terraform config with container registry, log analytics, and app services

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "log_analytics_workspace_id" {
-  value = azurerm_log_analytics_workspace.workspaceaicloudbuilder9db5.id
-}
-
 output "container_registry_id" {
   value = azurerm_container_registry.spiritops.id
 }
 
-output "managed_environment_id" {
-  value = azurerm_app_managed_environment.spiritops_container_app_env.id
+output "log_analytics_workspace_id" {
+  value = azurerm_log_analytics_workspace.workspaceaicloudbuilder9db5.id
 }
 
 output "container_app_id" {
   value = azurerm_container_app.spiritops_app.id
 }
 
-output "managed_certificate_id" {
-  value = azurerm_app_managed_certificate.spiritops_managed_certificate.id
+output "app_service_environment_id" {
+  value = azurerm_app_service_environment.spiritops_container_app_env.id
 }
 
 output "dns_zone_id" {

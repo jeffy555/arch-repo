3ba0a851012bafada960b33bcac8bca92051414f
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-03-31 23:53:48 +0530

    feat: update Terraform config with container registry, log analytics, and app services

--- main.tf
+++ main.tf
@@ -2,36 +2,31 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_log_analytics_workspace" "workspaceaicloudbuilder9db5" {
-  name                = var.log_analytics_workspace_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.aicloudbuilder.name
-  sku                 = "PerGB2018"
-}
-
 resource "azurerm_container_registry" "spiritops" {
   name                = var.container_registry_name
-  location            = var.location
   resource_group_name = azurerm_resource_group.aicloudbuilder.name
+  location            = var.location
   sku                 = var.container_registry_sku
 }
 
-resource "azurerm_app_managed_environment" "spiritops_container_app_env" {
-  name                = var.managed_environment_name
-  location            = var.location
+resource "azurerm_log_analytics_workspace" "workspaceaicloudbuilder9db5" {
+  name                = var.log_analytics_workspace_name
   resource_group_name = azurerm_resource_group.aicloudbuilder.name
+  location            = var.location
+  sku                 = "PerGB2018"
 }
 
 resource "azurerm_container_app" "spiritops_app" {
   name                = var.container_app_name
-  location            = var.location
   resource_group_name = azurerm_resource_group.aicloudbuilder.name
-  managed_environment_id = azurerm_app_managed_environment.spiritops_container_app_env.id
+  location            = var.location
+  managed_environment_id = azurerm_app_service_environment.spiritops_container_app_env.id
 }
 
-resource "azurerm_app_managed_certificate" "spiritops_managed_certificate" {
-  name                = var.managed_certificate_name
-  managed_environment_id = azurerm_app_managed_environment.spiritops_container_app_env.id
+resource "azurerm_app_service_environment" "spiritops_container_app_env" {
+  name                = var.app_service_environment_name
+  resource_group_name = azurerm_resource_group.aicloudbuilder.name
+  location            = var.location
 }
 
 resource "azurerm_dns_zone" "spiritops_in" {

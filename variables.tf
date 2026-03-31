5c799a0fe425813ae035bd4b50b2d8161ee6ec62
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-03-31 23:53:50 +0530

    feat: update Terraform config with container registry, log analytics, and app services

--- variables.tf
+++ variables.tf
@@ -1,47 +1,41 @@
-variable "log_analytics_workspace_name" {
-  description = "The name of the Log Analytics Workspace"
-  type        = string
-  default     = "workspaceaicloudbuilder9db5"
-}
-
 variable "container_registry_name" {
-  description = "The name of the Container Registry"
+  description = "The name of the Azure Container Registry."
   type        = string
   default     = "spiritops"
 }
 
-variable "managed_environment_name" {
-  description = "The name of the Managed Environment"
+variable "log_analytics_workspace_name" {
+  description = "The name of the Log Analytics Workspace."
   type        = string
-  default     = "spiritops-container-app-env"
+  default     = "workspaceaicloudbuilder9db5"
 }
 
 variable "container_app_name" {
-  description = "The name of the Container App"
+  description = "The name of the Container App."
   type        = string
   default     = "spiritops-app"
 }
 
-variable "managed_certificate_name" {
-  description = "The name of the Managed Certificate"
+variable "app_service_environment_name" {
+  description = "The name of the App Service Environment."
   type        = string
-  default     = "www.spiritops.in-spiritop-260227063125"
+  default     = "spiritops-container-app-env"
 }
 
 variable "dns_zone_name" {
-  description = "The name of the DNS Zone"
+  description = "The name of the DNS Zone."
   type        = string
   default     = "spiritops.in"
 }
 
 variable "location" {
-  description = "The Azure location"
+  description = "The Azure location where resources will be created."
   type        = string
   default     = "southindia"
 }
 
 variable "container_registry_sku" {
-  description = "The SKU of the Container Registry"
+  description = "The SKU of the Container Registry."
   type        = string
   default     = "Basic"
 }

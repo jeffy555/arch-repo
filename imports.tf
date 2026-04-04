6c9577331aeed852981a19e98083cfc2c58441a8
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-04 17:02:06 +0530

    feat: update Azure resources for container app and managed environment setup

--- imports.tf
+++ imports.tf
@@ -2,8 +2,8 @@
 # Review and adjust mapping before terraform apply.
 
 import {
-  to = azurerm_log_analytics_workspace.workspace
-  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.OperationalInsights/workspaces/workspaceaicloudbuilder9db5"
+  to = azurerm_app_managed_environment.spiritops_container_app_env
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env"
 }
 
 import {
@@ -12,18 +12,18 @@ import {
 }
 
 import {
-  to = azurerm_container_registry.spiritops_registry
+  to = azurerm_container_registry.spiritops
   id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.ContainerRegistry/registries/spiritops"
 }
 
 import {
-  to = azurerm_app_managed_environment.spiritops_env
-  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env/managedCertificates/www.spiritops.in-spiritop-260227063125"
+  to = azurerm_log_analytics_workspace.workspaceaicloudbuilder9db5
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.OperationalInsights/workspaces/workspaceaicloudbuilder9db5"
 }
 
 import {
   to = azurerm_app_managed_certificate.spiritops_certificate
-  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env"
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env/managedCertificates/www.spiritops.in-spiritop-260227063125"
 }
 
 import {

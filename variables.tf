74d7f1682614680160490709fee0ae287f2e12ed
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-04 13:20:56 +0530

    feat: update Azure resources including container registry, log analytics, and app services

--- variables.tf
+++ variables.tf
@@ -4,18 +4,6 @@ variable "container_registry_name" {
   default     = "spiritops"
 }
 
-variable "managed_environment_name" {
-  description = "The name of the Azure Managed Environment."
-  type        = string
-  default     = "spiritops-container-app-env"
-}
-
-variable "certificate_name" {
-  description = "The name of the Managed Certificate."
-  type        = string
-  default     = "www.spiritops.in-spiritop-260227063125"
-}
-
 variable "log_analytics_workspace_name" {
   description = "The name of the Log Analytics Workspace."
   type        = string
@@ -28,6 +16,12 @@ variable "container_app_name" {
   default     = "spiritops-app"
 }
 
+variable "managed_environment_name" {
+  description = "The name of the Managed Environment."
+  type        = string
+  default     = "spiritops-container-app-env"
+}
+
 variable "dns_zone_name" {
   description = "The name of the DNS Zone."
   type        = string
@@ -35,13 +29,25 @@ variable "dns_zone_name" {
 }
 
 variable "location" {
-  description = "The Azure location for the resources."
+  description = "The Azure location where resources will be created."
   type        = string
   default     = "southindia"
 }
 
 variable "container_registry_sku" {
-  description = "The SKU for the Container Registry."
+  description = "The SKU of the Container Registry."
   type        = string
   default     = "Basic"
+}
+
+variable "certificate_name" {
+  description = "The name of the Managed Certificate."
+  type        = string
+  default     = "www.spiritops.in-spiritop-260227063125"
+}
+
+variable "certificate_hostname" {
+  description = "The hostname for the Managed Certificate."
+  type        = string
+  default     = "www.spiritops.in"
 }

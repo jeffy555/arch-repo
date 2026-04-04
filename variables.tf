9c18bab5ecc7296507cab3657d6f4e2dc82034c7
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-04 12:34:01 +0530

    feat: update Azure resources including container registry and DNS zone

--- variables.tf
+++ variables.tf
@@ -1,19 +1,25 @@
-variable "log_analytics_workspace_name" {
-  description = "The name of the Log Analytics workspace."
-  type        = string
-  default     = "workspaceaicloudbuilder9db5"
-}
-
 variable "container_registry_name" {
-  description = "The name of the Container Registry."
+  description = "The name of the Azure Container Registry."
   type        = string
   default     = "spiritops"
 }
 
-variable "container_registry_sku" {
-  description = "The SKU of the Container Registry."
+variable "managed_environment_name" {
+  description = "The name of the Azure Managed Environment."
   type        = string
-  default     = "Basic"
+  default     = "spiritops-container-app-env"
+}
+
+variable "certificate_name" {
+  description = "The name of the Managed Certificate."
+  type        = string
+  default     = "www.spiritops.in-spiritop-260227063125"
+}
+
+variable "log_analytics_workspace_name" {
+  description = "The name of the Log Analytics Workspace."
+  type        = string
+  default     = "workspaceaicloudbuilder9db5"
 }
 
 variable "container_app_name" {
@@ -22,38 +28,20 @@ variable "container_app_name" {
   default     = "spiritops-app"
 }
 
-variable "managed_environment_name" {
-  description = "The name of the Managed Environment."
-  type        = string
-  default     = "spiritops-container-app-env"
-}
-
-variable "managed_certificate_name" {
-  description = "The name of the Managed Certificate."
-  type        = string
-  default     = "www.spiritops.in-spiritop-260227063125"
-}
-
-variable "certificate_hostname" {
-  description = "The hostname for the Managed Certificate."
-  type        = string
-  default     = "www.spiritops.in"
-}
-
 variable "dns_zone_name" {
   description = "The name of the DNS Zone."
   type        = string
   default     = "spiritops.in"
 }
 
-variable "dns_zone_location" {
-  description = "The location of the DNS Zone."
-  type        = string
-  default     = "global"
-}
-
 variable "location" {
-  description = "The Azure location where resources will be created."
+  description = "The Azure location for the resources."
   type        = string
   default     = "southindia"
+}
+
+variable "container_registry_sku" {
+  description = "The SKU for the Container Registry."
+  type        = string
+  default     = "Basic"
 }

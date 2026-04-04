43ea2e530658704e5e3b6932a3f6b8aa7f3b8c4a
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-04 14:26:38 +0530

    feat: Update Azure resources for Container App, Registry, and DNS zone

--- variables.tf
+++ variables.tf
@@ -1,53 +1,53 @@
-variable "container_registry_name" {
-  description = "The name of the Azure Container Registry."
-  type        = string
-  default     = "spiritops"
-}
-
-variable "log_analytics_workspace_name" {
-  description = "The name of the Log Analytics Workspace."
-  type        = string
-  default     = "workspaceaicloudbuilder9db5"
-}
-
-variable "container_app_name" {
+variable "spiritops_app_name" {
   description = "The name of the Container App."
   type        = string
   default     = "spiritops-app"
 }
 
-variable "managed_environment_name" {
+variable "spiritops_registry_name" {
+  description = "The name of the Container Registry."
+  type        = string
+  default     = "spiritops"
+}
+
+variable "workspace_name" {
+  description = "The name of the Log Analytics Workspace."
+  type        = string
+  default     = "workspaceaicloudbuilder9db5"
+}
+
+variable "spiritops_container_app_env_name" {
   description = "The name of the Managed Environment."
   type        = string
   default     = "spiritops-container-app-env"
 }
 
-variable "dns_zone_name" {
+variable "spiritops_certificate_name" {
+  description = "The name of the Managed Certificate."
+  type        = string
+  default     = "www.spiritops.in-spiritop-260227063125"
+}
+
+variable "spiritops_dnszone_name" {
   description = "The name of the DNS Zone."
   type        = string
   default     = "spiritops.in"
 }
 
 variable "location" {
-  description = "The Azure location where resources will be created."
+  description = "The Azure location for resources."
   type        = string
   default     = "southindia"
 }
 
-variable "container_registry_sku" {
-  description = "The SKU of the Container Registry."
+variable "spiritops_registry_sku" {
+  description = "The SKU for the Container Registry."
   type        = string
   default     = "Basic"
 }
 
-variable "certificate_name" {
-  description = "The name of the Managed Certificate."
+variable "workspace_sku" {
+  description = "The SKU for the Log Analytics Workspace."
   type        = string
-  default     = "www.spiritops.in-spiritop-260227063125"
-}
-
-variable "certificate_hostname" {
-  description = "The hostname for the Managed Certificate."
-  type        = string
-  default     = "www.spiritops.in"
+  default     = "PerGB2018"
 }

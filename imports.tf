6e360ab9f92c1c29cda4850e308743c79a7a6478
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-04 16:57:23 +0530

    feat: update Terraform configuration with Azure resources for SpiritOps

--- imports.tf
+++ imports.tf
@@ -2,26 +2,31 @@
 # Review and adjust mapping before terraform apply.
 
 import {
+  to = azurerm_log_analytics_workspace.workspace
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.OperationalInsights/workspaces/workspaceaicloudbuilder9db5"
+}
+
+import {
   to = azurerm_container_app.spiritops_app
   id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/containerApps/spiritops-app"
 }
 
 import {
-  to = azurerm_app_managed_environment.spiritops_container_app_env
-  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env/managedCertificates/www.spiritops.in-spiritop-260227063125"
-}
-
-import {
-  to = azurerm_container_registry.spiritops
+  to = azurerm_container_registry.spiritops_registry
   id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.ContainerRegistry/registries/spiritops"
 }
 
 import {
-  to = azurerm_log_analytics_workspace.workspaceaicloudbuilder9db5
+  to = azurerm_app_managed_environment.spiritops_env
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env/managedCertificates/www.spiritops.in-spiritop-260227063125"
+}
+
+import {
+  to = azurerm_app_managed_certificate.spiritops_certificate
   id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env"
 }
 
 import {
-  to = azurerm_dns_zone.spiritops_in
-  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.OperationalInsights/workspaces/workspaceaicloudbuilder9db5"
+  to = azurerm_dns_zone.spiritops_dns_zone
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.Network/dnszones/spiritops.in"
 }

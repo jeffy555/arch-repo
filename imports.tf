166d7588721cdc16ebe24be698792b4351cc20cb
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-04 14:26:37 +0530

    feat: Update Azure resources for Container App, Registry, and DNS zone

--- imports.tf
+++ imports.tf
@@ -1,4 +1,9 @@
 import {
+  to = azurerm_container_app.spiritops_app
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/containerApps/spiritops-app"
+}
+
+import {
   to = azurerm_container_registry.spiritops
   id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.ContainerRegistry/registries/spiritops"
 }
@@ -9,21 +14,16 @@ import {
 }
 
 import {
-  to = azurerm_container_app.spiritops_app
-  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/containerApps/spiritops-app"
-}
-
-import {
   to = azurerm_app_managed_environment.spiritops_container_app_env
   id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env"
 }
 
 import {
-  to = azurerm_dns_zone.spiritops_in
-  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.Network/dnszones/spiritops.in"
+  to = azurerm_app_managed_certificate.spiritops_certificate
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env/managedCertificates/www.spiritops.in-spiritop-260227063125"
 }
 
 import {
-  to = azurerm_app_managed_certificate.spiritops_certificate
-  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env/managedCertificates/www.spiritops.in-spiritop-260227063125"
+  to = azurerm_dns_zone.spiritops_dnszone
+  id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.Network/dnszones/spiritops.in"
 }

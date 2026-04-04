6688fea63b08efdbba09afde851be7c214efa16e
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-04 14:26:38 +0530

    feat: Update Azure resources for Container App, Registry, and DNS zone

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "container_app_id" {
+  value = azurerm_container_app.spiritops_app.id
+}
+
 output "container_registry_id" {
   value = azurerm_container_registry.spiritops.id
 }
@@ -6,18 +10,10 @@ output "log_analytics_workspace_id" {
   value = azurerm_log_analytics_workspace.workspaceaicloudbuilder9db5.id
 }
 
-output "container_app_id" {
-  value = azurerm_container_app.spiritops_app.id
-}
-
 output "managed_environment_id" {
   value = azurerm_app_managed_environment.spiritops_container_app_env.id
 }
 
 output "dns_zone_id" {
-  value = azurerm_dns_zone.spiritops_in.id
-}
-
-output "managed_certificate_id" {
-  value = azurerm_app_managed_certificate.spiritops_certificate.id
+  value = azurerm_dns_zone.spiritops_dnszone.id
 }

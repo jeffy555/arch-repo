436ef6c0e6e5995c445c5c17c7d2f480d5465397
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-04 19:54:51 +0530

    feat: update infrastructure with new resources and configurations in Azure

--- main.tf
+++ main.tf
@@ -1,183 +1,116 @@
 resource "azurerm_resource_group" "migrate_scope" {
-  name     = "AICloudBuilder"
-  location = "southindia"
+  name     = "myresume-live-rg"
+  location = "centralindia"
 }
 
-resource "azurerm_container_app_environment" "spiritops_env" {
-  name                = "spiritops-container-app-env"
-  location            = azurerm_resource_group.migrate_scope.location
+resource "azurerm_storage_account" "preacherjefferson" {
+  name                     = "preacherjefferson"
+  resource_group_name      = azurerm_resource_group.migrate_scope.name
+  location                 = "centralindia"
+  account_tier             = "Standard"
+  account_replication_type = "RAGRS"
+  allow_blob_public_access = true
+  allow_shared_key_access  = true
+  min_tls_version          = "TLS1_2"
+  enable_https_traffic_only = true
+  large_file_share_enabled = true
+
+  network_rules {
+    default_action             = "Allow"
+    bypass                     = ["AzureServices"]
+  }
+
+  encryption {
+    services {
+      blob {
+        enabled = true
+      }
+      file {
+        enabled = true
+      }
+    }
+    key_source = "Microsoft.Storage"
+  }
+}
+
+resource "azurerm_application_insights" "jeffersonimmanuel" {
+  name                = "jeffersonimmanuel"
+  location            = "centralus"
   resource_group_name = azurerm_resource_group.migrate_scope.name
-  app_logs_configuration {
-    destination = "log-analytics"
-    log_analytics_configuration {
-      customer_id = "a87dc595-9854-44aa-b93d-3ae1fed44d82"
-    }
-  }
-  dapr_configuration {
-    version = "1.13.6-msft.6"
-  }
-  workload_profiles {
-    name                = "Consumption"
-    workload_profile_type = "Consumption"
-  }
-  custom_domain_configuration {
-    custom_domain_verification_id = "10B2983330597EAC9D66E1F4DD670F7B3F3F9434BD322CF27D25196B36498F79"
-  }
+  application_type    = "web"
 }
 
-resource "azurerm_container_app" "spiritops_app" {
-  name                    = "spiritops-app"
-  location                = azurerm_resource_group.migrate_scope.location
-  resource_group_name     = azurerm_resource_group.migrate_scope.name
-  container_app_environment_id = azurerm_container_app_environment.spiritops_env.id
-  revision_mode           = "Single"
-
-  template {
-    container {
-      name   = "spiritops-app"
-      image  = "spiritops.azurecr.io/spiritops-app:284"
-      cpu    = 0.5
-      memory = "1Gi"
-
-      env {
-        name  = "NODE_ENV"
-        value = "production"
-      }
-      env {
-        name  = "PORT"
-        value = "9005"
-      }
-      env {
-        name      = "DATABASE_URL"
-        secret_ref = "database-url"
-      }
-      env {
-        name      = "JWT_SECRET"
-        secret_ref = "jwt-secret"
-      }
-      env {
-        name      = "OPENAI_API_KEY"
-        secret_ref = "openai-api-key"
-      }
-      env {
-        name      = "BITWARDEN_ACCESS_TOKEN"
-        secret_ref = "bitwarden-access-token"
-      }
-      env {
-        name      = "BITWARDEN_PROJECT_ID"
-        secret_ref = "bitwarden-project-id"
-      }
-
-      probes {
-        type                = "Liveness"
-        failure_threshold   = 3
-        period_seconds      = 10
-        success_threshold   = 1
-        timeout_seconds     = 5
-        tcp_socket {
-          port = 23040
-        }
-      }
-      probes {
-        type                = "Readiness"
-        failure_threshold   = 48
-        period_seconds      = 5
-        success_threshold   = 1
-        timeout_seconds     = 5
-        tcp_socket {
-          port = 23040
-        }
-      }
-      probes {
-        type                = "Startup"
-        failure_threshold   = 240
-        initial_delay_seconds = 1
-        period_seconds      = 1
-        success_threshold   = 1
-        timeout_seconds     = 3
-        tcp_socket {
-          port = 23040
-        }
-      }
-    }
-
-    scale {
-      min_replicas = 4
-      max_replicas = 10
-      rule {
-        name = "http-scaler"
-        http {
-          metadata = {
-            concurrentRequests = "10"
-          }
-        }
-      }
-    }
-  }
-
-  ingress {
-    external = true
-    target_port = 0
-    exposed_port = 0
-    transport = "Auto"
-    traffic {
-      weight = 100
-      latest_revision = true
-    }
-    custom_domain {
-      name = "www.spiritops.in"
-      certificate_id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env/managedCertificates/www.spiritops.in-spiritop-260227063125"
-      binding_type = "SniEnabled"
-    }
-    allow_insecure = false
-    sticky_sessions {
-      affinity = "none"
-    }
-  }
-}
-
-resource "azurerm_log_analytics_workspace" "workspace" {
-  name                = "workspaceaicloudbuilder9db5"
-  location            = azurerm_resource_group.migrate_scope.location
+resource "azurerm_app_service_plan" "asp_myresumeliverg_ade0" {
+  name                = "ASP-myresumeliverg-ade0"
+  location            = "centralus"
   resource_group_name = azurerm_resource_group.migrate_scope.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-  public_network_access_for_ingestion = "Enabled"
-  public_network_access_for_query     = "Enabled"
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
 }
 
-resource "azurerm_container_registry" "spiritops" {
-  name                = "spiritops"
-  location            = azurerm_resource_group.migrate_scope.location
+resource "azurerm_virtual_network" "myresume_live_rg_vnet" {
+  name                = "myresume-live-rg-vnet"
+  location            = "centralindia"
   resource_group_name = azurerm_resource_group.migrate_scope.name
-  sku                 = "Basic"
-  admin_enabled       = true
-  public_network_access = "Enabled"
 
-  retention_policy {
-    days    = 7
-    status  = "disabled"
-  }
+  address_space = ["10.224.0.0/12"]
 
-  trust_policy {
-    type   = "Notary"
-    status = "disabled"
-  }
+  subnet {
+    name           = "default"
+    address_prefix = "10.224.0.0/16"
 
-  quarantine_policy {
-    status = "disabled"
-  }
-
-  export_policy {
-    status = "enabled"
-  }
-
-  azure_ad_authentication_as_arm_policy {
-    status = "enabled"
+    service_endpoints = ["Microsoft.ContainerRegistry"]
   }
 }
 
-resource "azurerm_dns_zone" "spiritops_in" {
-  name                = "spiritops.in"
+resource "azurerm_monitor_action_group" "application_insights_smart_detection" {
+  name                = "Application Insights Smart Detection"
   resource_group_name = azurerm_resource_group.migrate_scope.name
+  short_name          = "SmartDetect"
+
+  arm_role_receiver {
+    role_id               = "749f88d5-cbae-40b8-bcfc-e573ddc772fa"
+    use_common_alert_schema = true
+  }
+
+  arm_role_receiver {
+    role_id               = "43d0d8ad-25c7-4714-9337-8ba259a9fe05"
+    use_common_alert_schema = true
+  }
 }
+
+resource "azurerm_monitor_action_group" "recommended_alert_rules_ag_6b460f" {
+  name                = "RecommendedAlertRules-AG-6b460f"
+  resource_group_name = azurerm_resource_group.migrate_scope.name
+  short_name          = "alert6b460f"
+
+  email_receiver {
+    name                    = "Email_-EmailAction-"
+    email_address           = "jeffersonimmanuel5@gmail.com"
+    use_common_alert_schema = true
+  }
+}
+
+resource "azurerm_linux_web_app" "jeffersonimmanuel" {
+  name                = "jeffersonimmanuel"
+  location            = "centralus"
+  resource_group_name = azurerm_resource_group.migrate_scope.name
+  service_plan_id     = azurerm_app_service_plan.asp_myresumeliverg_ade0.id
+
+  site_config {
+    linux_fx_version = "NODE|22-lts"
+  }
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  }
+
+  https_only = true
+}
+
+# migrateops: TODO - Add azurerm_container_app and azurerm_container_app_environment resources if needed

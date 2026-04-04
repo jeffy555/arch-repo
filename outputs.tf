6b2ca475528af7f118d0d31497708e3973745b4f
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-04 13:20:55 +0530

    feat: update Azure resources including container registry, log analytics, and app services

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,6 @@ output "container_registry_id" {
   value = azurerm_container_registry.spiritops.id
 }
 
-output "managed_environment_id" {
-  value = azurerm_app_managed_environment.spiritops_container_app_env.id
-}
-
-output "certificate_id" {
-  value = azurerm_app_managed_certificate.spiritops_certificate.id
-}
-
 output "log_analytics_workspace_id" {
   value = azurerm_log_analytics_workspace.workspaceaicloudbuilder9db5.id
 }
@@ -18,6 +10,14 @@ output "container_app_id" {
   value = azurerm_container_app.spiritops_app.id
 }
 
+output "managed_environment_id" {
+  value = azurerm_app_managed_environment.spiritops_container_app_env.id
+}
+
 output "dns_zone_id" {
-  value = azurerm_dns_zone.spiritops_dns_zone.id
+  value = azurerm_dns_zone.spiritops_in.id
+}
+
+output "managed_certificate_id" {
+  value = azurerm_app_managed_certificate.spiritops_certificate.id
 }

e5f03c50d76ea1ade8a6b19ed4957cfeae6c2ba5
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-04 23:02:20 +0530

    feat: update Terraform configuration with new Azure resources and settings

--- main.tf
+++ main.tf
@@ -1,114 +1,185 @@
 resource "azurerm_resource_group" "migrate_scope" {
-  name     = "myresume-live-rg"
-  location = "global"
+  name     = "AICloudBuilder"
+  location = "southindia"
 }
 
-resource "azurerm_monitor_action_group" "application_insights_smart_detection" {
-  name                = "Application Insights Smart Detection"
+resource "azurerm_container_registry" "spiritops" {
+  name                = "spiritops"
+  location            = "southindia"
   resource_group_name = azurerm_resource_group.migrate_scope.name
-  location            = "global"
+  sku                 = "Basic"
 
-  arm_role_receiver {
-    name                   = "Monitoring Contributor"
-    role_id                = "749f88d5-cbae-40b8-bcfc-e573ddc772fa"
-    use_common_alert_schema = true
+  admin_enabled = true
+
+  georeplication_locations = []
+
+  retention_policy {
+    days    = 7
+    enabled = false
   }
 
-  arm_role_receiver {
-    name                   = "Monitoring Reader"
-    role_id                = "43d0d8ad-25c7-4714-9337-8ba259a9fe05"
-    use_common_alert_schema = true
+  trust_policy {
+    type    = "Notary"
+    enabled = false
   }
+
+  export_policy {
+    enabled = true
+  }
+
+  public_network_access_enabled = true
 }
 
-resource "azurerm_application_insights" "jeffersonimmanuel" {
-  name                = "jeffersonimmanuel"
-  location            = "centralus"
+resource "azurerm_log_analytics_workspace" "workspaceaicloudbuilder9db5" {
+  name                = "workspaceaicloudbuilder9db5"
+  location            = "southindia"
   resource_group_name = azurerm_resource_group.migrate_scope.name
-  application_type    = "web"
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  public_network_access_for_ingestion = "Enabled"
+  public_network_access_for_query     = "Enabled"
 }
 
-resource "azurerm_storage_account" "preacherjefferson" {
-  name                     = "preacherjefferson"
-  location                 = "centralindia"
-  resource_group_name      = azurerm_resource_group.migrate_scope.name
-  account_tier             = "Standard"
-  account_replication_type = "RAGRS"
+resource "azurerm_dns_zone" "spiritops_in" {
+  name                = "spiritops.in"
+  resource_group_name = azurerm_resource_group.migrate_scope.name
+}
 
-  allow_nested_items_to_be_public = true
-  https_traffic_only_enabled      = true
-  shared_access_key_enabled       = true
+resource "azurerm_container_app_environment" "spiritops_container_app_env" {
+  name                = "spiritops-container-app-env"
+  location            = "southindia"
+  resource_group_name = azurerm_resource_group.migrate_scope.name
 
-  min_tls_version = "TLS1_2"
-
-  blob_properties {
-    delete_retention_policy {
-      days = 7
+  app_logs_configuration {
+    destination = "log-analytics"
+    log_analytics_configuration {
+      customer_id = "a87dc595-9854-44aa-b93d-3ae1fed44d82"
     }
   }
 
-  network_rules {
-    default_action             = "Allow"
-    bypass                     = ["AzureServices"]
-    ip_rules                   = []
-    virtual_network_subnet_ids = []
+  workload_profiles {
+    name = "Consumption"
+    type = "Consumption"
   }
 
-  identity {
-    type = "SystemAssigned"
-  }
+  zone_redundant = false
 }
 
-resource "azurerm_virtual_network" "myresume_live_rg_vnet" {
-  name                = "myresume-live-rg-vnet"
-  location            = "centralindia"
+resource "azurerm_container_app" "spiritops_app" {
+  name                = "spiritops-app"
+  location            = "southindia"
   resource_group_name = azurerm_resource_group.migrate_scope.name
-  address_space       = ["10.224.0.0/12"]
+  container_app_environment_id = azurerm_container_app_environment.spiritops_container_app_env.id
 
-  subnet {
-    name                      = "default"
-    address_prefixes          = ["10.224.0.0/16"]
-    private_endpoint_network_policies = "Disabled"
-    private_link_service_network_policies_enabled = true
-    service_endpoints         = ["Microsoft.ContainerRegistry"]
-  }
-}
+  revision_mode = "Single"
 
-resource "azurerm_monitor_action_group" "recommended_alert_rules_ag_6b460f" {
-  name                = "RecommendedAlertRules-AG-6b460f"
-  resource_group_name = azurerm_resource_group.migrate_scope.name
-  location            = "global"
+  template {
+    container {
+      name  = "spiritops-app"
+      image = "spiritops.azurecr.io/spiritops-app:284"
+      cpu   = 0.5
+      memory = "1Gi"
 
-  email_receiver {
-    name                   = "Email_-EmailAction-"
-    email_address          = "jeffersonimmanuel5@gmail.com"
-    use_common_alert_schema = true
-  }
-}
+      env {
+        name  = "NODE_ENV"
+        value = "production"
+      }
 
-resource "azurerm_service_plan" "asp_myresumeliverg_ade0" {
-  name                = "ASP-myresumeliverg-ade0"
-  location            = "centralus"
-  resource_group_name = azurerm_resource_group.migrate_scope.name
-  os_type             = "Linux"
-  sku_name            = "F1"
-}
+      env {
+        name  = "PORT"
+        value = "9005"
+      }
 
-resource "azurerm_linux_web_app" "jeffersonimmanuel" {
-  name                = "jeffersonimmanuel"
-  location            = "centralus"
-  resource_group_name = azurerm_resource_group.migrate_scope.name
-  service_plan_id     = azurerm_service_plan.asp_myresumeliverg_ade0.id
+      env {
+        name      = "DATABASE_URL"
+        secret_ref = "database-url"
+      }
 
-  site_config {
-    application_stack {
-      node_version = "22-lts"
+      env {
+        name      = "JWT_SECRET"
+        secret_ref = "jwt-secret"
+      }
+
+      env {
+        name      = "OPENAI_API_KEY"
+        secret_ref = "openai-api-key"
+      }
+
+      env {
+        name      = "BITWARDEN_ACCESS_TOKEN"
+        secret_ref = "bitwarden-access-token"
+      }
+
+      env {
+        name      = "BITWARDEN_PROJECT_ID"
+        secret_ref = "bitwarden-project-id"
+      }
+
+      probes {
+        type                = "Liveness"
+        failure_threshold   = 3
+        period_seconds      = 10
+        success_threshold   = 1
+        timeout_seconds     = 5
+        tcp_socket {
+          port = 23040
+        }
+      }
+
+      probes {
+        type                = "Readiness"
+        failure_threshold   = 48
+        period_seconds      = 5
+        success_threshold   = 1
+        timeout_seconds     = 5
+        tcp_socket {
+          port = 23040
+        }
+      }
+
+      probes {
+        type                = "Startup"
+        failure_threshold   = 240
+        initial_delay_seconds = 1
+        period_seconds      = 1
+        success_threshold   = 1
+        timeout_seconds     = 3
+        tcp_socket {
+          port = 23040
+        }
+      }
+    }
+
+    scale {
+      min_replicas = 4
+      max_replicas = 10
+
+      rule {
+        name = "http-scaler"
+        http {
+          metadata = {
+            concurrentRequests = "10"
+          }
+        }
+      }
     }
   }
 
-  https_only = true
-
-  tags = {
-    "hidden-link: /app-insights-resource-id" = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/myresume-live-rg/providers/microsoft.insights/components/jeffersonimmanuel"
+  ingress {
+    external = true
+    target_port = 0
+    exposed_port = 0
+    transport = "Auto"
+    traffic {
+      weight = 100
+      latest_revision = true
+    }
+    custom_domain {
+      name = "www.spiritops.in"
+      certificate_id = "/subscriptions/be1b0fcb-1e30-4142-bb0c-ff52f7a1a0e5/resourceGroups/AICloudBuilder/providers/Microsoft.App/managedEnvironments/spiritops-container-app-env/managedCertificates/www.spiritops.in-spiritop-260227063125"
+      binding_type = "SniEnabled"
+    }
+    allow_insecure = false
   }
 }

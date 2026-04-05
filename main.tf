2a3eacc5338fd14b8747537f709afd483a52229c
jeffy555 <jeffersonimmanuel5@gmail.com>
2026-04-05 12:23:24 +0530

    feat: update Azure resources including container registry and app environment

--- main.tf
+++ main.tf
@@ -3,15 +3,6 @@ resource "azurerm_resource_group" "migrate_scope" {
   location = "southindia"
 }
 
-resource "azurerm_log_analytics_workspace" "workspaceaicloudbuilder9db5" {
-  name                = "workspaceaicloudbuilder9db5"
-  location            = "southindia"
-  resource_group_name = azurerm_resource_group.migrate_scope.name
-  sku                 = "PerGB2018"
-  internet_ingestion_enabled = true
-  internet_query_enabled     = true
-}
-
 resource "azurerm_container_registry" "spiritops" {
   name                = "spiritops"
   location            = "southindia"
@@ -19,10 +10,17 @@ resource "azurerm_container_registry" "spiritops" {
   sku                 = "Basic"
   admin_enabled       = true
   export_policy_enabled = true
-  public_network_access_enabled = true
   retention_policy_in_days = 7
+  public_network_access_enabled = true
   network_rule_bypass_option = "AzureServices"
-  zone_redundancy_enabled = false
+}
+
+resource "azurerm_log_analytics_workspace" "workspaceaicloudbuilder9db5" {
+  name                = "workspaceaicloudbuilder9db5"
+  location            = "southindia"
+  resource_group_name = azurerm_resource_group.migrate_scope.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
 
 resource "azurerm_dns_zone" "spiritops_in" {
@@ -36,54 +34,20 @@ resource "azurerm_container_app_environment" "spiritops_container_app_env" {
   resource_group_name = azurerm_resource_group.migrate_scope.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.workspaceaicloudbuilder9db5.id
   zone_redundancy_enabled = false
-  # infrastructure_subnet_id = "<subnet_id>" # Commented out due to invalid value
   workload_profile {
-    name                = "Consumption"
+    name                 = "Consumption"
     workload_profile_type = "Consumption"
   }
 }
 
 resource "azurerm_container_app" "spiritops_app" {
   name                = "spiritops-app"
+  location            = "southindia"
   resource_group_name = azurerm_resource_group.migrate_scope.name
   container_app_environment_id = azurerm_container_app_environment.spiritops_container_app_env.id
   revision_mode       = "Single"
   max_inactive_revisions = 100
   workload_profile_name = "Consumption"
-
-  secret {
-    name = "spiritopsazurecrio-spiritops"
-  }
-  secret {
-    name = "bitwarden-access-token"
-  }
-  secret {
-    name = "bitwarden-project-id"
-  }
-  secret {
-    name = "database-url"
-  }
-  secret {
-    name = "jwt-secret"
-  }
-  secret {
-    name = "openai-api-key"
-  }
-
-  ingress {
-    target_port = 0
-    external_enabled = true
-    traffic_weight {
-      percentage = 100
-      latest_revision = true
-    }
-  }
-
-  registry {
-    server = "spiritops.azurecr.io"
-    username = "spiritops"
-    password_secret_name = "spiritopsazurecrio-spiritops"
-  }
 
   template {
     min_replicas = 4
@@ -104,49 +68,37 @@ resource "azurerm_container_app" "spiritops_app" {
         value = "9005"
       }
       env {
-        name = "DATABASE_URL"
+        name       = "DATABASE_URL"
         secret_name = "database-url"
       }
       env {
-        name = "JWT_SECRET"
+        name       = "JWT_SECRET"
         secret_name = "jwt-secret"
       }
       env {
-        name = "OPENAI_API_KEY"
+        name       = "OPENAI_API_KEY"
         secret_name = "openai-api-key"
       }
       env {
-        name = "BITWARDEN_ACCESS_TOKEN"
+        name       = "BITWARDEN_ACCESS_TOKEN"
         secret_name = "bitwarden-access-token"
       }
       env {
-        name = "BITWARDEN_PROJECT_ID"
+        name       = "BITWARDEN_PROJECT_ID"
         secret_name = "bitwarden-project-id"
       }
 
       liveness_probe {
-        port = 23040
+        port     = 23040
         transport = "tcp"
-        failure_count_threshold = 3
-        interval_seconds = 10
-        timeout = 5
       }
-
       readiness_probe {
-        port = 23040
+        port     = 23040
         transport = "tcp"
-        failure_count_threshold = 48
-        interval_seconds = 5
-        timeout = 5
       }
-
       startup_probe {
-        port = 23040
+        port     = 23040
         transport = "tcp"
-        failure_count_threshold = 240
-        initial_delay = 1
-        interval_seconds = 1
-        timeout = 3
       }
     }
 
@@ -154,5 +106,39 @@ resource "azurerm_container_app" "spiritops_app" {
       name = "http-scaler"
       concurrent_requests = "10"
     }
+  }
+
+  ingress {
+    target_port = 0
+    external_enabled = true
+    traffic_weight {
+      percentage = 100
+      latest_revision = true
+    }
+  }
+
+  registry {
+    server = "spiritops.azurecr.io"
+    username = "spiritops"
+    password_secret_name = "spiritopsazurecrio-spiritops"
+  }
+
+  secret {
+    name = "spiritopsazurecrio-spiritops"
+  }
+  secret {
+    name = "bitwarden-access-token"
+  }
+  secret {
+    name = "bitwarden-project-id"
+  }
+  secret {
+    name = "database-url"
+  }
+  secret {
+    name = "jwt-secret"
+  }
+  secret {
+    name = "openai-api-key"
   }
 }
